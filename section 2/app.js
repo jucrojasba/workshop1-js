2bd0f71cf7f49360412491a44a257a12bcf48509
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-04-18 20:43:19 -0500

    Add section 2 file app.js

---
+++ section 2/app.js
@@ -0,0 +1,20 @@
+//Hola Mundo
+/**Taylor Swift es la mejor en la industria pop
+ * Mi cancion favorita de ella al momento es Cruel Summer
+ * o en general cualquiera del album Lover
+ */
+
+const miNumero = 10;
+console.log(miNumero);
+const miBooleano = true;
+console.log(typeof miBooleano);
+const miCadena = "Hola Mundo ";
+console.log(miCadena.length);
+const miArray = ["Hola", 23, ["mundo"]];
+console.log(miArray);
+const miObjeto = {
+  name: "Federico",
+  age: 23,
+  job: "Engineer",
+};
+console.log(miObjeto);
